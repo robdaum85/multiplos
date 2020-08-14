2dfb169afa3cd8cc1cfe50f5877b6135c46eb1db
robson <robdaum85@gmail.com>
2020-08-14 19:14:49 -0300

    Primeira inserção

--- multiplos/Program.cs
+++ multiplos/Program.cs
@@ -10,6 +10,24 @@ namespace multiplos
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Multiplos de 3 e 5 - avaliação de teste");
+
+            for (var i = 1; i <= 100; i++)
+            {
+                Console.Write(i);
+                if (i % 3 == 0)
+                {
+                    Console.Write("ROBSON");
+                }
+
+                if (i % 5 == 0)
+                {
+                    Console.Write("JUNIOR");
+                }
+                Console.WriteLine("  ");
+            }
+            Console.ReadKey();
         }
     }
 }
